07a1e67ea52dd605cc3c65737c99cdfe2dfb8baf
amritasrc <amritakumari13022006@gmail.com>
2026-07-08 13:54:12 +0530

    chore: add logo

--- src/comps/Navbar.jsx
+++ src/comps/Navbar.jsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between bg-gray-800 p-4 text-white">
-      <h1 className="text-xl font-bold">CopyIt</h1>
+    <nav className="flex items-center justify-between bg-zinc-950 p-4 text-white">
+      <div className="flex items-center gap-2 justify-between">
+        <img src="./favicon.jpeg" className="w-9" alt="" />
+        <h1 className="text-xl font-bold">CopyIt</h1>
+      </div>
 
       <div className="flex gap-6">
         <Link to="/">Dashboard</Link>
