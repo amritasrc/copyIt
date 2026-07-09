8e4b039090ccd9f404a6a19f0fffd1eca9351a29
amritasrc <amritakumari13022006@gmail.com>
2026-07-09 22:24:14 +0530

    feat: configure routes and main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import Navbar from './comps/Navbar'
-import { BrowserRouter } from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Navbar from './comps/Navbar';
+import Home from './pages/Home';
+import CreateSnippet from './pages/CreateSnippet';
+import ViewSnippet from './pages/ViewSnippet';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='min-h-screen w-full bg-zinc-950 text-zinc-100'>
+      <div className="min-h-screen bg-zinc-950 text-zinc-100 flex flex-col">
         <Navbar />
+        <main className="flex-1 bg-">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<CreateSnippet />} />
+            <Route path="/snippet/:id" element={<ViewSnippet />} />
+          </Routes>
+        </main>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App
