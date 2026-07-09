a0d47f4028518caad9b7c2ab9deabecd9d663707
amritasrc <amritakumari13022006@gmail.com>
2026-07-09 21:43:53 +0530

    feat: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Navbar from './comps/Navbar'
+import { BrowserRouter } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <div className='min-h-screen w-full bg-zinc-950 text-zinc-100'>
+        <Navbar />
+      </div>
+    </BrowserRouter>
   )
 }
 
