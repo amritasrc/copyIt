2bafff5c02ec1e39337a5065ad274636ed32d55b
Amrita <amritakumari13022006@gmail.com>
2026-07-25 23:30:06 +0530

    fix: logo outline fixed

--- src/comps/Navbar.jsx
+++ src/comps/Navbar.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 import { HiPlus } from "react-icons/hi";
 
 const Navbar = () => {
-    return (
-        <nav className='bg-zinc-950 border-b border-zinc-700 px-6 py-4 flex justify-between items-center'>
-            <Link to="/" className='flex items-center gap-2 text-xl font-bold text-zinc-50'>
-                <img src="/favicon.jpeg"
-                    className='w-7'
-                    alt="logo" />
-                <span>CopyIt</span>
-            </Link>
-            <Link to="/create" className='flex items-center gap-1 bg-purple-600 hover:bg-purple-500 px-4 py-2 rounded-lg text-sm font-medium transition'>
-                <HiPlus />
-                Create Snippet
-            </Link>
-        </nav>
-    )
-}
+  return (
+    <nav className="bg-zinc-950 border-b border-zinc-700 px-6 py-4 flex justify-between items-center">
+      <Link
+        to="/"
+        className="flex items-center gap-2 text-xl font-bold text-zinc-50"
+      >
+        <img src="/favicon.jpeg" className="w-7 rounded-lg" alt="logo" />
+        <span>CopyIt</span>
+      </Link>
+      <Link
+        to="/create"
+        className="flex items-center gap-1 bg-purple-600 hover:bg-purple-500 px-4 py-2 rounded-lg text-sm font-medium transition"
+      >
+        <HiPlus />
+        Create Snippet
+      </Link>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
