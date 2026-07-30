7b560db4f46b2e2cced31c612cb00ef886ff7a70
ankitsensei <ankitbhagat.dev@gmail.com>
2026-07-30 20:22:14 +0530

    style: homepage responsive

--- src/comps/SnippetCard.jsx
+++ src/comps/SnippetCard.jsx
@@ -3,7 +3,7 @@ import { HiEye, HiTrash } from "react-icons/hi";
 
 const SnippetCard = ({ id, title, description, lang, code, onDelete }) => {
   return (
-    <div className="flex flex-col justify-between w-100 rounded-xl border border-zinc-700 bg-zinc-800 p-5 shadow-sm transition hover:border-indigo-500 hover:shadow-lg">
+    <div className="flex flex-col justify-between rounded-xl border border-zinc-700 bg-zinc-800 p-5 shadow-sm transition hover:border-indigo-500 hover:shadow-lg">
       <div>
         <div className="mb-2 flex items-start justify-between">
           <h3 className="max-w-[70%] truncate text-lg font-semibold text-white">
