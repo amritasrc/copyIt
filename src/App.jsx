b0500e35c1a6b87f554956f4166c272a573a7357
amritasrc <amritakumari13022006@gmail.com>
2026-07-30 19:47:29 +0530

    feat: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import supabase from './lib/supabase'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/create" element={<CreateSnippet />} />
-      <Route path="/snippet/:id" element={<ViewSnippet />} />
-      <Route path='/edit' element={<EditSnippet />} /> */}
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
     </Routes>
   )
 }
