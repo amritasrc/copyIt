2be06397dfdb539cb879c5b2eda01bf0715ab3be
amritasrc <amritakumari13022006@gmail.com>
2026-08-01 20:25:40 +0530

    feat: login/logout based on current user

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './comps/Navbar'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import { useDispatch } from 'react-redux'
+import authService from './lib/auth'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Footer from './comps/Footer'
 import { Provider } from 'react-redux'
 import store from './store/store'
 import CreateSnippet from './pages/CreateSnippet'
+import { login, logout } from './store/authSlice'
 
 const App = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+  },[])
+
   return (
     <BrowserRouter>
-      <Provider store={store}>
         <Navbar />
         <main className='flex-1'>
           <Routes>
@@ -23,7 +40,6 @@ const App = () => {
           </Routes>
         </main>
         <Footer />
-      </Provider>
     </BrowserRouter>
   )
 }
