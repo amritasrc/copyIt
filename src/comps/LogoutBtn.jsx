d4e46e4a8098b90a2796dde07e20cd009ef8ed76
amritasrc <amritakumari13022006@gmail.com>
2026-08-03 20:39:53 +0530

    style: logout btn

--- src/comps/LogoutBtn.jsx
+++ src/comps/LogoutBtn.jsx
@@ -12,7 +12,7 @@ const LogoutBtn = () => {
     }
     return (
         <button
-            className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+            className='inline-block cursor-pointer px-6 py-2 hover:bg-zinc-100 hover:text-black transition duration-200 rounded-full'
             onClick={logoutHandler}
         >Logout</button>
     )
