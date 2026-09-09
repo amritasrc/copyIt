e8722656150b9e810c2763106ed180f526bd265a
amritasrc <amritakumari13022006@gmail.com>
2026-09-09 20:23:04 +0530

    chore: deleted everything wanna start from scratch using mern

--- src/lib/supabase.js
+++
@@ -1,9 +0,0 @@
-import conf from "../config/conf";
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-    conf.supabaseUrl,
-    conf.supabaseAnonKey
-);
-
-export default supabase;
