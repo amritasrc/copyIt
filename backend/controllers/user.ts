59e63509f76a409d5cd74c7f1c0ede6389dde50f
amritasrc <amritakumari13022006@gmail.com>
2026-09-10 22:07:33 +0530

    feat: create controller for user model

---
+++ backend/controllers/user.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import User from "../models/user";
+
+async function handleCreateNewUser(req:Request, res:Response) {
+    const { username, email, password } = req.body;
+
+    await User.create({
+        username: username,
+        email: email,
+        password: password
+    })
+
+    res.status(201).json({
+        success: true,
+        message: "User created successfully"
+    });
+}
+
+export default handleCreateNewUser;
