1afeda789f0e262ee77f66c68a19da411320bd57
amritasrc <amritakumari13022006@gmail.com>
2026-09-11 11:18:38 +0530

    feat: store hashed password instead of original

--- backend/controllers/user.ts
+++ backend/controllers/user.ts
@@ -1,13 +1,15 @@
 import { Request, Response } from "express";
 import User from "../models/user.js";
+import bcrypt from 'bcrypt';
 
 async function handleCreateNewUser(req:Request, res:Response) {
     const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     await User.create({
         username: username,
         email: email,
-        password: password
+        password: hashedPassword,
     })
 
     res.status(201).json({
