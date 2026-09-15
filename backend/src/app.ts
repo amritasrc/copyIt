ad919000ed95f7125d1422e08b3cb6689fffa810
amritasrc <amritakumari13022006@gmail.com>
2026-09-15 20:24:25 +0530

    chore: install types/cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import router from './routes/user.js'
 import snippetRouter from "./routes/snippet.js";
 import errorHandler from "./middlewares/errorHandler.js";
@@ -7,6 +8,11 @@ const app = express();
 
 app.use(express.urlencoded());
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use('/api/users', router);
 app.use('/api/snippets', snippetRouter);
