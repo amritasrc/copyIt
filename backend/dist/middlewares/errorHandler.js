d81bd068f075895e3d83a7a73a89a53dd73e259d
amritasrc <amritakumari13022006@gmail.com>
2026-09-20 22:17:32 +0530

    fix: configure production API and CORS

--- backend/dist/middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-function errorHandler(err, req, res, next) {
-    console.error(err);
-    res.status(500).json({
-        success: false,
-        message: "Something went wrong",
-    });
-}
-export default errorHandler;
